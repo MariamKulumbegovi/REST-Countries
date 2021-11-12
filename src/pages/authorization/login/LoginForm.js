be49b295c10a02db086623d229b2d1554ff87b1f
MariamK <78789152+MariamKulumbegovi@users.noreply.github.com>
2021-11-12 19:43:49 +0400

    .

--- src/pages/authorization/login/LoginForm.js
+++ src/pages/authorization/login/LoginForm.js
@@ -6,55 +6,55 @@ import {ASKQUESTION_PATH} from "../../../components/constants/routes"
 export const LoginForm = () => {
 
    
-    const history=useHistory()
+    // const history=useHistory()
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+    // const onSubmit = (event) => {
+    //     event.preventDefault();
     
-        const fd = new FormData(event.target);
-        const loginData = {};
+    //     const fd = new FormData(event.target);
+    //     const loginData = {};
     
-        for (let [key, value] of fd.entries()) {
-          loginData[key] = value;
-        }
+    //     for (let [key, value] of fd.entries()) {
+    //       loginData[key] = value;
+    //     }
     
-        fetch(`${process.env.REACT_APP_API_URL}/login`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify(loginData),
-        })
-          .then((res) => res.json())
-          .then((result) => {
-            if(result.token){
-                history.replace(ASKQUESTION_PATH)
-            }
-        })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+    //     fetch(`${process.env.REACT_APP_API_URL}/login`, {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Accept: "application/json",
+    //       },
+    //       body: JSON.stringify(loginData),
+    //     })
+    //       .then((res) => res.json())
+    //       .then((result) => {
+    //         if(result.token){
+    //             history.replace(ASKQUESTION_PATH)
+    //         }
+    //     })
+    //       .catch((error) => {
+    //         console.log(error);
+    //       });
+    //   };
     
-    return (
+    // return (
     
-    <form className="content" onSubmit={onSubmit}>
-        <div className="field">
-        <EmailInput/>
+    // <form className="content" onSubmit={onSubmit}>
+    //     <div className="field">
+    //     <EmailInput/>
            
-        </div>
-        <div className="field">
-            <PasswordInput/>
-        </div>
-        <p className="control">
-            <button className="button is-danger">
-                    Login
-            </button>
-         </p>
-     </form>
+    //     </div>
+    //     <div className="field">
+    //         <PasswordInput/>
+    //     </div>
+    //     <p className="control">
+    //         <button className="button is-danger">
+    //                 Login
+    //         </button>
+    //      </p>
+    //  </form>
     
-    )
+    // )
  }
 
 
